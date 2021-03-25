b9ea8e4845654564bed83b7b639422e82b8da365
Nicolas-villarraga <80805713+Nicolas-villarraga@users.noreply.github.com>
2021-03-24 19:47:32 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>hola mundo </h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium vero tempore blanditiis eum exercitationem dolore earum adipisci ab aut distinctio voluptatem consequuntur laborum sapiente, quia recusandae. Cupiditate, architecto nesciunt. Totam.</p>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
